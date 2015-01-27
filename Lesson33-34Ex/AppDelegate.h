4097d33a13a9dd0e040e400e9fb435c004757dcf
Roman Jouravkov <roma1990may@rambler.ru>
2015-01-27 15:45:58 +0300

    Initial Commit

---
+++ Lesson33-34Ex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson33-34Ex
+//
+//  Created by Hopreeeeenjust on 27.01.15.
+//  Copyright (c) 2015 Hopreeeeenjust. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
